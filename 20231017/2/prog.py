2cb92fb5f358f8ca883abee34e881dc0c51b0799
Valery Zabrovsky <valthebrewer@yandex.ru>
2023-10-21 12:58:45 +0300

    Modify 20231017/2/prog.py string parsing method

--- 20231017/2/prog.py
+++ 20231017/2/prog.py
@@ -1,23 +1,13 @@
 from math import *
 
-def qsplit(s):
-    s += ' '
-    qt = ''
-    ans = []
-    last = 0
-    for i in range(len(s)):
-        if not qt and s[i].isspace():
-            if last != i:
-                ans.append(s[last : i])
-            last = i + 1
-        elif not qt and s[i] in ("'", '"'):
-            qt = s[i]
-        elif s[i] == qt:
-            qt = ''
-    return ans
-
 ctr = 0
 funcs = {'quit': ['msg', 'msg.format(len(funcs), ctr)']}
+
+def qsplit(s):
+    ans = s.split()
+    if ans[0][0] != ':' and len(funcs[ans[0]]) == 2 and ans[1][0] in ('"', "'"):
+        return s.split(maxsplit=1)
+    return ans
 
 while s := qsplit(input()):
     ctr += 1
