930de6bb960858152ed9c3cb5c4c77314b5cac49
Valery Zabrovsky <valthebrewer@yandex.ru>
2023-12-19 14:08:00 +0300

    [Multiple Fixes] Seminar: December 19, 2023

--- 20231017/2/prog.py
+++ 20231017/2/prog.py
@@ -5,10 +5,10 @@ funcs = {'quit': ['msg', 'msg.format(len(funcs), ctr)']}
 
 def qsplit(s):
     ans = s.split()
-    if ans[0][0] != ':'
+    if (ans[0][0] != ':'
             and len(funcs[ans[0]]) == 2
             and ans[1][0] in ('"', "'")
-            and ans[1][0] == ans[-1][-1]:
+            and ans[1][0] == ans[-1][-1]):
         return s.split(maxsplit=1)
     return ans
 
